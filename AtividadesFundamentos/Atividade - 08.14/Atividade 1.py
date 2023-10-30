46d5a104feb290a802392ba7d12ae186416bb122
defprofeta <abrahaofcon@gmail.com>
2023-10-29 21:42:52 -0300

    adicionando meses

--- AtividadesFundamentos/Atividade - 08.14/Atividade 1.py
+++
@@ -1,6 +0,0 @@
-print ("Atividade 1 - n1")
-
-print ("Abrahão")
-
-nome = input("escreva seu nome: ")
-print (nome)
--- AtividadesFundamentos/Atividade - 09.08/Numero2.py
+++
@@ -1,8 +0,0 @@
-'''2. Solicite um número inteiro "n" e imprima na tela se ele é par ou ímpar;'''
-
-n = int(input('Digite um número: '))
-
-if n % 2 == 0:
-    print('É par')
-else:
-    print('É ímpar')
