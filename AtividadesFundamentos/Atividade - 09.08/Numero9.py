71ca5c093e2b49708657186fa55c6601ddf3ec3a
defprofeta <abrahaofcon@gmail.com>
2023-10-29 21:40:55 -0300

    Atualização aula 8 / Setembro

--- AtividadesFundamentos/Atividade - 09.08/Numero9.py
+++ AtividadesFundamentos/Atividade - 09.08/Numero9.py
@@ -2,16 +2,29 @@
 dados, mas receba apenas 2 e calcule o terceiro. Os dados devem ser números fracionários, e o dado a ser
 calculado deve ser informado como vazio, apenas pressionando ENTER.'''
 
+
 x = input('Digite a velocidade: ')
 y = input('Digite o tempo: ')
 z = input('Digite a distância: ')
 
-if x == '\n':
+
+if x == '':
+    z = float(z)
+    y = float(y)
+
     calculoX = float(z / y)
     print('A velocidade é: ', calculoX)
-if y == '\n':
+
+if y == '':
+    z = float(z)
+    y = float(x)
+
     calculoY = float(x / z)
     print('O tempo é: ', calculoY)
-if z == '\n':
+
+if z == '':
+    z = float(x)
+    y = float(y)
+
     calculoZ = float(x * y)
     print('A distância é: ', calculoZ)
