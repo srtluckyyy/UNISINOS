9b5cea00b2817a532825efdc083dcaed5de2b8ca
defprofeta <abrahaofcon@gmail.com>
2023-10-31 02:35:32 -0300

    aula 18 set

--- AtividadesFundamentos/5. Atividade - set.18/4 .py
+++ AtividadesFundamentos/5. Atividade - set.18/4 .py
@@ -5,24 +5,33 @@ parâmetros separadamente. Essa função deverá retornar um número float
 representando as horas, minutos e segundos como um número fracionário. Ex:
 “01:15:30” = 1,2583 ou “13:20:15” = 13,3375. Imprima o número fracionário a partir
 do “main”;
-
 '''
 
 def _horaParaFloat(hora, minuto, segundo):
-    pass
+    soma = hora + (minuto / 60) + (segundo / 3600)
+    return soma
 
 
 if __name__ == '__main__':
-    print('Informe a hora atual(hh:mm:ss): \n')
+    print('Informe a hora atual: \n')
 
-    h = input()
-    vetorHorario = []
+    h = int(input('hora '))
+    m = int(input('minuto '))
+    s = int(input('segundo '))
     i = 0
 
-    for n in range(0, len(h), 3):
-        vetorHorario.append(h[i:n + 1])
-        i += 3
-        
-    print(vetorHorario)
-    
-    pass
+    while i == 0:
+        if s >= 60:
+            s -= 60
+            m += 1
+        elif m >= 60:
+            m -= 60
+            h += 1
+        else:
+            i = 1
+            break
+
+    print(h, ':', m, ':', s)
+
+    soma = _horaParaFloat(h, m, s)
+    print('O float da hora é: %1.4f'%soma)
