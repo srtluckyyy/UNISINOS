8892bfb371f46b593ef6a6f65442757adf1764a7
defprofeta <abrahaofcon@gmail.com>
2023-11-20 14:43:40 -0300

    0

--- AtividadesFundamentos/testes/testeIn.py
+++
@@ -1,7 +0,0 @@
-x = ['a', 'b', 'c']
-y = 'a'
-print('{} in {}: {}'.format(y, x, y in x))
-print('{} not in {}: {}'.format(y, x, y not in x))
-y = 'd'
-print('{} in {}: {}'.format(y, x, y in x))
-print('{} not in {}: {}'.format(y, x, y not in x))
